8b0a50f24fac0baeef2881a4477e6ecc0f929411
jhnbg rth <I1234itsplaz@outlook.com>
2023-01-06 21:30:49 -0500

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,1 +1,32 @@
-
+var menu1 = document.getElementById("menu1")
+var menu2 = document.getElementById("menu2")
+var menu3 = document.getElementById("menu3")
+var menu4 = document.getElementById("menu4")
+function changeMenu(n) {
+ switch(n) {
+   case "1":
+      menu1.style.display = block
+      menu2.style.display = hidden
+      menu3.style.display = hidden
+      menu4.style.display = hidden
+     break;
+   case "2":
+      menu1.style.display = hidden
+      menu2.style.display = block
+      menu3.style.display = hidden
+      menu4.style.display = hidden
+     break;
+   case "3":
+      menu1.style.display = hidden
+      menu2.style.display = hidden
+      menu3.style.display = block
+      menu4.style.display = hidden
+     break;
+   case "4":
+      menu1.style.display = hidden
+      menu2.style.display = hidden
+      menu3.style.display = hidden
+      menu4.style.display = block
+     break;
+ }
+}
